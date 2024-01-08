2fcd735da1e88258f29a5b6dc7c02647482a8eaf
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-01-08 15:38:30 +0800

    wip

--- resources/views/infolists/components/project-division-details.blade.php
+++ resources/views/infolists/components/project-division-details.blade.php
@@ -6,16 +6,16 @@
 
     <div class="container mx-auto mt-8">
         @foreach ($getRecord()->project_years as $project_year)
-            <div class="year-container border text-sm border-gray-500 mt-2 rounded-lg">
+            <div class="year-container border text-xs border-gray-500 mt-2 rounded-lg">
                 <p class="year-title text-lg font-bold p-2 text-center">
                     {{ $project_year->year->title }}
                 </p>
 
-                <table class="w-full border text-sm border-gray-500">
+                <table class="w-full border text-xs border-gray-500">
                     <thead>
-                        <tr   class="border text-sm border-gray-500 px-4 py-2">
+                        <tr class="border text-xs border-gray-500 px-4 py-2 ">
                             <!-- Common Headers -->
-                            <th>Quarter</th>
+                            <th class="py-2 " >Quarters</th>
 
                         </tr>
                     </thead>
@@ -24,46 +24,46 @@
                             <tr>
                                 <!-- Quarter -->
                                 <td
-                                    class="border text-sm border-gray-500 px-4 py-2">
+                                    class="border text-xs border-gray-500 px-4 py-2 text-md  text-primary-600">
                                     {{ $project_quarter->quarter->title }}
                                 </td>
                                 @foreach ($project_quarter->project_divisions as $project_division)
                                     <tr>
                                         <!-- Division -->
                                         <td
-                                            class="border text-sm border-gray-500 px-4 py-2">
+                                            class="border text-xs border-gray-500 px-4 py-2 font-bold">
                                             {{ $project_division->division->title }}
                                         </td>
                                         @foreach ($project_division->project_division_categories as $project_division_category)
                                         <tr>
                                             <!-- Category -->
-                                            <td class="border text-sm border-gray-500 px-4 py-2">
+                                            <td class="border text-xs border-gray-500 px-4 py-2">
                                                 {{ $project_division_category->from }}
                                             </td>
                                             <!-- Subcategory and Expense Table -->
-                                            <td class="border text-sm border-gray-500 px-4 py-2">
-                                                <table class="border text-sm border-gray-500 w-full" >
+                                            <td class="border text-xs border-gray-500 px-4 py-2">
+                                                <table class="border text-xs border-gray-500 w-full" >
                                                     @foreach ($project_division_category->project_division_sub_category_expenses as $thirdlayer)
                                                         <tr>
                                                             <!-- Subcategory and Expense Details -->
-                                                            <td class="border text-sm border-gray-500 px-4 py-2">
+                                                            <td class="border text-xs border-gray-500 px-4 py-2">
                                                                 @if ($project_division_category->from === 'Indirect Cost')
                                                                     {{ $thirdlayer->parent_title }}
                                                                 @endif
                                                                 {{ $thirdlayer->title }}
                                                             </td>
-                                                            <td class="border text-sm border-gray-500 px-4 py-2">
+                                                            <td class="border text-xs border-gray-500 px-4 py-2">
                                                                 {{-- {{ $thirdlayer}} --}}
                                                             </td>
                                                         </tr>
                                                         @foreach ($thirdlayer->fourth_layers as $fourthlayer)
                                                             <!-- Fourth Layer Details -->
                                                             <tr>
-                                                                <td class="border text-sm border-gray-500 px-8 py-2"></td>
-                                                                <td class="border text-sm border-gray-500 px-8 py-2">
+                                                                <td class="border text-xs border-gray-500 px-8 py-2"></td>
+                                                                <td class="border text-xs border-gray-500 px-8 px-4 py-2">
                                                                     {{ $fourthlayer->title }}
                                                                 </td>
-                                                                <td class="border text-sm border-gray-500 px-8 py-2">
+                                                                <td class="border text-xs border-gray-500 px-8 px-4 py-2">
                                                                     {{ number_format($fourthlayer->amount) }}
                                                                 </td>
                                                             </tr>
@@ -91,7 +91,7 @@
     {{--
          <div class="container mx-auto mt-8">
         @foreach ($getRecord()->project_years as $project_year)
-            <div class="year-container border text-sm border-gray-500 mt-2 rounded-lg">
+            <div class="year-container border text-xs border-gray-500 mt-2 rounded-lg">
                 <p class="year-title text-lg font-bold p-2 text-center">
                     {{ $project_year->year->title }}
                 </p>
@@ -99,11 +99,11 @@
                 <table class="w-full">
                     <thead>
                         <tr>
-                            <th class="border text-sm border-gray-500 px-4 py-2">Quarter</th>
-                            <th class="border text-sm border-gray-500 px-4 py-2">Division</th>
-                            <th class="border text-sm border-gray-500 px-4 py-2">Category</th>
-                            <th class="border text-sm border-gray-500 px-4 py-2">Subcategory</th>
-                            <th class="border text-sm border-gray-500 px-4 py-2">Expense</th>
+                            <th class="border text-xs border-gray-500 px-4 py-2">Quarter</th>
+                            <th class="border text-xs border-gray-500 px-4 py-2">Division</th>
+                            <th class="border text-xs border-gray-500 px-4 py-2">Category</th>
+                            <th class="border text-xs border-gray-500 px-4 py-2">Subcategory</th>
+                            <th class="border text-xs border-gray-500 px-4 py-2">Expense</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -111,38 +111,38 @@
                             @foreach ($project_quarter->project_divisions as $first)
                                 <tr>
                                     <td rowspan="{{ $first->project_division_categories->count() }}"
-                                        class="border text-sm border-gray-500 px-4 py-2">
+                                        class="border text-xs border-gray-500 px-4 py-2">
                                         @if ($loop->first)
                                             {{ $project_quarter->quarter->title }}
                                         @endif
                                     </td>
                                     <td rowspan="{{ $first->project_division_categories->count() }}"
-                                        class="border text-sm border-gray-500 px-4 py-2">
+                                        class="border text-xs border-gray-500 px-4 py-2">
                                         {{ $first->division->title }}
                                     </td>
                                     @foreach ($first->project_division_categories as $secondlayer)
                                         @foreach ($secondlayer->project_division_sub_category_expenses as $thirdlayer)
                                             <tr>
-                                                <td class="border text-sm border-gray-500 px-4 py-2">
+                                                <td class="border text-xs border-gray-500 px-4 py-2">
                                                     {{ $secondlayer->from }}
                                                 </td>
-                                                <td class="border text-sm border-gray-500 px-4 py-2">
+                                                <td class="border text-xs border-gray-500 px-4 py-2">
                                                     @if ($secondlayer->from === 'Indirect Cost')
                                                         {{ $thirdlayer->parent_title }}
                                                     @endif
                                                     {{ $thirdlayer->title }}
                                                 </td>
-                                                <td class="border text-sm border-gray-500 px-4 py-2">
+                                                <td class="border text-xs border-gray-500 px-4 py-2">
                                                     {{ number_format($thirdlayer->fourth_layers->sum('amount')) }}
                                                 </td>
                                             </tr>
                                             @foreach ($thirdlayer->fourth_layers as $fourthlayer)
                                                 <tr>
-                                                    <td class="border text-sm border-gray-500 px-8 py-2"></td>
-                                                    <td class="border text-sm border-gray-500 px-8 py-2">
+                                                    <td class="border text-xs border-gray-500 px-8 py-2"></td>
+                                                    <td class="border text-xs border-gray-500 px-8 py-2">
                                                         {{ $fourthlayer->title }}
                                                     </td>
-                                                    <td class="border text-sm border-gray-500 px-8 py-2">
+                                                    <td class="border text-xs border-gray-500 px-8 py-2">
                                                         {{ number_format($fourthlayer->amount) }}
                                                     </td>
                                                 </tr>
