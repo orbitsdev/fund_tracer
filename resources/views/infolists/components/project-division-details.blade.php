d4401211d51292024a57d52de85826b585d16a65
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-01-15 09:29:46 +0800

    wip

--- resources/views/infolists/components/project-division-details.blade.php
+++ resources/views/infolists/components/project-division-details.blade.php
@@ -1,58 +1,73 @@
 
 <x-dynamic-component :component="$getEntryWrapperView()" :entry="$entry">
 
-    <div class="border text-xs border-gray-500 px-4 py-2 mt-4">
-        <h1 class="text-lg font-bold">{{ $getRecord()->title }}</h1>
+
+
+    <div class=" mt-4">
 
         @foreach ($getRecord()->project_years as $projectYear)
-            <div class="border text-xs border-gray-500 px-4 py-2 mt-4">
-                <p class="text-md font-bold">{{ $projectYear->year->title }}</p>
+            <div class="border rounded-lg text-xs border-gray-500 px-4 py-2 mt-4">
+                <p class="text-md font-medium">{{ $projectYear->year->title }}</p>
+
+                <div class="">
 
                 @foreach ($projectYear->project_quarters as $projectQuarter)
-                    <div class="border text-xs border-gray-500 px-4 py-2 ml-4 mt-2">
-                        <p class="text-md font-bold">{{ $projectQuarter->quarter->title }}</p>
+                    <div class=" border text-xs border-gray-500 px-4 py-2 ml-4 mt-2">
+                        <p class="text-md font-bold ">{{ $projectQuarter->quarter->title }}</p>
 
-                        <table class="border-collapse border text-xs border-gray-500 w-full">
-                            <thead>
-                                <tr>
-                                    <th class="border text-xs border-gray-500 px-4 py-2">Division</th>
-                                    <th class="border text-xs border-gray-500 px-4 py-2">Parent Title</th>
-                                    <th class="border text-xs border-gray-500 px-4 py-2">Category Type</th>
-                                    <th class="border text-xs border-gray-500 px-4 py-2">Expense Title</th>
-                                    <th class="border text-xs border-gray-500 px-4 py-2">Amount</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($projectQuarter->quarter_expense_budget_divisions as $budgetDivision)
-                                <tr>
-                                    <td class="border text-xs border-gray-500 px-4 py-2">{{ $budgetDivision->project_division->division->title }}</td>
-                                    <td class="border text-xs border-gray-500 px-4 py-2" rowspan="{{ count($budgetDivision->quarter_expenses) }}">{{ $budgetDivision->project_division->division->title }}</td>
-                                    @foreach ($budgetDivision->quarter_expenses as $expenses)
-                                        <td class="border text-xs border-gray-500 px-4 py-2">{{ $expenses->fourth_layer->project_division_sub_category_expense->parent_title }}</td>
-                                        <td class="border text-xs border-gray-500 px-4 py-2">{{ $expenses->fourth_layer->project_division_sub_category_expense->project_division_category->from }}</td>
-                                        <td class="border text-xs border-gray-500 px-4 py-2">{{ $expenses->fourth_layer->title }}</td>
-                                        <td class="border text-xs border-gray-500 px-4 py-2">{{ number_format($expenses->amount) }}</td>
-                                    </tr>
-                                    <tr>
-                                        <!-- Nested row for 'from' column -->
-                                        <td class="border text-xs border-gray-500 px-4 py-2"></td>
-                                        <td class="border text-xs border-gray-500 px-4 py-2" colspan="4">{{ $expenses->fourth_layer->project_division_sub_category_expense->from }}</td>
-                                    </tr>
-                                @endforeach
+                        @foreach ($projectQuarter->quarter_expense_budget_divisions as $budgetDivision)
+                            <div class="border text-xs border-gray-500 px-4 py-2 ml-4 mt-2">
+                                <p class="text-md  font-bold">{{ $budgetDivision->project_division->division->title }}</p>
+                                @foreach ($budgetDivision->quarter_expenses->groupBy('fourth_layer.project_division_sub_category_expense.parent_title') as $parentTitle => $expensesGroup)
+                                <p class="text-md font-bold">{{ $parentTitle }}</p>
+                                <p class="text-xs  " style="margin-left: 8px;" >{{ $expensesGroup->first()->fourth_layer->project_division_sub_category_expense->project_division_category->from }}</p>
+                                <p class="text-xs "  style="margin-left: 16px;">{{ $expensesGroup->first()->fourth_layer->project_division_sub_category_expense->title }}</p>
+                                @foreach ($expensesGroup as $expenses)
+                                <div class=" text-xs border-gray-500 px-4 py-2 ml-4 "  style="margin-left: 24px;">
+                                    <p class="text-md ">{{ $expenses->fourth_layer->title }}</p>
+                                    <p class="text-xs ">{{ $expenses->title }}</p>
+                                    <p class="text-xs  px-4 py-2 ">
+                                        {{ number_format($expenses->amount) }}
+                                    </p>
+                                </div>
                             @endforeach
+                                {{-- @dump($expensesGroup) --}}
+                                {{-- <div class="border text-xs border-gray-500 px-4 py-2 ml-4 mt-2">
+                                        <p class="text-md font-bold">{{ $parentTitle }}</p>
+                                        <p class="text-xs font-bold">{{ $expensesGroup->first()->fourth_layer->project_division_sub_category_expense->title }}</p>
+                                        <p class="text-xs font-bold">{{ $expensesGroup->first()->fourth_layer->project_division_sub_category_expense->project_division_category->from }}</p>
+                                        @foreach ($expensesGroup as $expenses)
+                                            <div class="border text-xs border-gray-500 px-4 py-2 ml-4 mt-2">
+                                                <p class="text-md font-bold">{{ $expenses->fourth_layer->title }}</p>
+                                                <p class="text-xs font-bold">{{ $expenses->title }}</p>
+                                                <p class="border text-xs border-gray-500 px-4 py-2 font-bold">
+                                                    {{ number_format($expenses->amount) }}
+                                                </p>
+                                            </div>
+                                        @endforeach
 
-                                </tbody>
-                            </table>
-                        </div>
+
+                                        <div class="border text-xs border-gray-500 px-4 py-2 ml-4 mt-2">
+                                            <p class="text-md font-bold">Total</p>
+                                            <p class="border text-xs border-gray-500 px-4 py-2 font-bold">
+                                                {{ number_format($expensesGroup->sum('amount')) }}
+                                            </p>
+                                        </div>
+                                    </div> --}}
+                                @endforeach
+                            </div>
+                        @endforeach
+                    </div>
+
                     @endforeach
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
+    </div>
 
 
 
-
-    {{-- <div class="border text-xs border-gray-500 px-4 py-2 mt-4">
+     {{-- <div class="border text-xs border-gray-500 px-4 py-2 mt-4">
         <h1 class="text-lg font-bold">{{ $getRecord()->title }}</h1>
 
         @foreach ($getRecord()->project_years as $projectYear)
@@ -82,6 +97,5 @@
             </div>
         @endforeach
     </div> --}}
-
 
 </x-dynamic-component>
