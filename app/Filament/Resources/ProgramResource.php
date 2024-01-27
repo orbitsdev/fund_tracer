3ac006614dd35afc00a10accf85092482c79718f
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-01-27 11:30:14 +0800

    wip

--- app/Filament/Resources/ProgramResource.php
+++ app/Filament/Resources/ProgramResource.php
@@ -44,7 +44,7 @@ use Filament\Tables\Columns\Summarizers\Range;
 use Filament\Tables\Columns\Summarizers\Average;
 use App\Filament\Resources\ProgramResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Icetalker\FilamentTableRepeater\Forms\Components\TableRepeater;
+use Awcodes\FilamentTableRepeater\Components\TableRepeater;
 use App\Filament\Resources\ProgramResource\RelationManagers;
 
 
