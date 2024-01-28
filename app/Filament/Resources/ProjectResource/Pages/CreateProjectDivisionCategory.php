cf8436126c64a80b7fe4abcf399c3c744cbb25c3
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-01-29 07:49:04 +0800

    wip

--- app/Filament/Resources/ProjectResource/Pages/CreateProjectDivisionCategory.php
+++ app/Filament/Resources/ProjectResource/Pages/CreateProjectDivisionCategory.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Resources\ProjectResource\Pages;
 
+use Closure;
+use Filament\Forms\Get;
 use Filament\Forms\Form;
 use App\Models\ProjectDevision;
 use Filament\Resources\Pages\Page;
@@ -10,6 +12,7 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Contracts\HasForms;
 use App\Models\ProjectDivisionCategory;
 use Filament\Actions\Action as FAction;
+use Illuminate\Validation\Rules\Unique;
 use Filament\Notifications\Notification;
 use Filament\Actions\Contracts\HasActions;
 use App\Filament\Resources\ProjectResource;
@@ -90,7 +93,7 @@ class CreateProjectDivisionCategory extends Page implements HasForms,  HasAction
             ->send();
             $this->form->fill();
         // dd(ProjectResource::getUrl('manage-quarter-year', ['record'=>$this->record->project->id]));
-        return redirect(ProjectResource::getUrl('project-table-division-category', ['record' => $this->record->project->id]));
+        return redirect(ProjectResource::getUrl('project-table-division', ['record' => $this->record->project->id]));
         // dd($this->form->fill($this->data));
         // dd($this->form->getState());
     }
@@ -103,13 +106,32 @@ class CreateProjectDivisionCategory extends Page implements HasForms,  HasAction
                 Hidden::make('project_devision_id'),
 
 
-                  Select::make('from')
+                 Select::make('from')
                             ->label('Costing Type')
+                            ->required()
                             ->options([
 
                                 'Direct Cost' => 'Direct Cost',
                                 'Indirect Cost' => 'Indirect Cost',
-                 ])  
+                 ])
+                //  ->rules([
+                //     fn (Get $get): Closure => function (string $attribute, $value, Closure $fail) use ($get) {
+                //          $exist = $this->record->whereHas('project_division_categories', function($query) use($get){
+                //     $query->where('project_devision_id', $this->record->id)->where('from', $get('from'));
+                // })->exist();
+
+                // if($exist){
+                //     $fail("The {$attribute} is invalid.");
+                // }
+                //     },
+                // ])
+                // ->afterStateUpdated(function (HasForms $livewire, $component) { 
+                //     $livewire->validateOnly($component->getStatePath());
+                // }), 
+                 ->unique(ignoreRecord: true, modifyRuleUsing: function (Unique $rule, Get $get) {
+                    return $rule->where('from', $get('from'))->where('project_devision_id', $this->record->id);
+                }) 
+              
 
              
 
